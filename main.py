f6b5f9247e1a13eeeb93ced8a1548e96c4c42f57
Craig Robson <craig.robson1@ncl.ac.uk>
2021-08-19 10:26:56 +0100

    added debug parameter. reduced printed messages when not in debug mode

--- main.py
+++ main.py
@@ -45,8 +45,12 @@ def fetch_inputs():
         print('Warning! No data year passed. Using default of 2011.')
         data_year = '2011'
 
+    debug = getenv('debug')
+    if debug is None:
+        debug = False
+
     output_dir = 'data/outputs/'
-    return feature_layer, auth_password, auth_username, area_codes, scale, data_year, output_dir, output_name
+    return feature_layer, auth_password, auth_username, area_codes, scale, data_year, output_dir, output_name, debug
 
 
 def clear_download_directory():
@@ -67,11 +71,11 @@ def check_response(response):
     return
 
 
-def download_data(query, area_codes, auth_username, auth_password, url='https://www.nismod.ac.uk/api/data'):
+def download_data(query, area_codes, auth_username, auth_password, url='https://www.nismod.ac.uk/api/data', debug=False):
     """"""
 
     for code in area_codes:
-        print(code)
+        if debug: print(code)
         #rstring = '%s/mastermap/buildings?export_format=geojson-zip&scale=%s&area_codes=%s&building_year=2011' % (url, area_scale, ''.join(code))
         rstring = '%s/%s&area_codes=%s' % (url, query, ''.join(code))
         response = requests.get(rstring, auth=(auth_username, auth_password))
@@ -82,11 +86,11 @@ def download_data(query, area_codes, auth_username, auth_password, url='https://
         z = zipfile.ZipFile(io.BytesIO(response.content))
         z.extractall('output')
 
-    print('Downloaded all data')
+    if debug: print('Downloaded all data')
 
     # need to loop through all files and add them to a geo-dataframe
     first = True
-    print(glob.glob("output/*.geojson"))
+    if debug: print(glob.glob("output/*.geojson"))
     for file in glob.glob("output/*.geojson"):
 
         s_buf = io.StringIO()
@@ -99,26 +103,26 @@ def download_data(query, area_codes, auth_username, auth_password, url='https://
         # delete file to free up more memory
         os.remove(file)
 
-    print('Created geodataframe')
+    print('Information: Downloaded all data and rreated geodataframe.')
 
     return gdf
 
 
 def main():
 
-    feature_layer, auth_password, auth_username, area_codes, scale, data_year, output_dir, output_name = fetch_inputs()
+    feature_layer, auth_password, auth_username, area_codes, scale, data_year, output_dir, output_name, debug = fetch_inputs()
 
 
     url = 'https://www.nismod.ac.uk/api/data'
 
     area_codes = area_codes.split(',')
-    print(area_codes)
+    if debug: print(area_codes)
 
     if scale == 'gors':
         # get the lads in a GOR
         lads = []
         for gor in area_codes:
-            print(gor)
+            if debug: print(gor)
             rstring = '%s/boundaries/lads_in_gor?gor_codes=%s&export_format=geojson' % (url, gor)
             response = requests.get(rstring, auth=(auth_username, auth_password))
 
@@ -133,7 +137,7 @@ def main():
         lads = area_codes
 
     #print('Got list of LADs to process (%s)' %len(lads))
-    print(lads)
+    if debug: print(lads)
     msoa = []
     for lad in lads:
         rstring = '%s/boundaries/msoas_in_lad?area_codes=%s&export_format=geojson' % (url, lad)
@@ -147,6 +151,7 @@ def main():
             msoa.append(feat['properties']['msoa_code'])
 
 
+    print('Information: Got all area codes ready to run processing.')
     area_scale = 'msoa'
     area_codes = msoa
 
@@ -154,7 +159,7 @@ def main():
         rstring = 'mastermap/buildings?export_format=geojson-zip&scale=%s&building_year=%s' % (area_scale, data_year)
 
         # download data and build dataframe
-        gdf = download_data(query=rstring, area_codes=area_codes, auth_username=auth_username, auth_password=auth_password)
+        gdf = download_data(query=rstring, area_codes=area_codes, auth_username=auth_username, auth_password=auth_password, debug=debug)
 
         gdf = gdf.replace('NULL', '0')
         gdf['res_count'] = gdf['res_count'].astype('int64')
@@ -162,7 +167,7 @@ def main():
         gdf['number_of_floors'] = gdf['number_of_floors'].astype('int64')
         gdf.drop(['toid_number','height_toroofbase','floor_area','building_ratio','number_of_floors'], axis=1)
 
-        print('Saving output')
+        print('Information: Saving output - %s' %output_name)
         gdf.to_file(os.path.join(output_dir,'%s.gpkg' %output_name), driver="GPKG")
 
     elif feature_layer == 'water-bodies':
@@ -171,14 +176,14 @@ def main():
         rstring = 'mastermap/areas?classification_codes=%s&export_format=geojson-zip&scale=%s&year=%s' % (classification_codes, area_scale, data_year)
 
         # download data and build geodataframe
-        gdf = download_data(query=rstring, area_codes=area_codes, auth_username=auth_username, auth_password=auth_password)
+        gdf = download_data(query=rstring, area_codes=area_codes, auth_username=auth_username, auth_password=auth_password, debug=debug)
 
         # any processing required before saving the data file
         gdf = gdf.replace('NULL', '0')
         #gdf['res_count'] = gdf['res_count'].astype('int64')
         #gdf.drop([''], axis=1)
 
-        print('Saving output')
+        print('Information: Saving output - %s' %output_name)
         gdf.to_file(os.path.join(output_dir,'%s.gpkg' %output_name), driver="GPKG")
 
     elif feature_layer == 'developed-land':
@@ -189,43 +194,33 @@ def main():
         classification_codes, area_scale, data_year)
 
         # download data and build geodataframe
-        gdf = download_data(query=rstring, area_codes=area_codes, auth_username=auth_username, auth_password=auth_password)
+        gdf = download_data(query=rstring, area_codes=area_codes, auth_username=auth_username, auth_password=auth_password, debug=debug)
 
-        print(gdf.columns)
-        #print(gdf.head)
-        print(len(gdf.index))
+        if debug: print(gdf.columns)
+        if debug: print(len(gdf.index))
 
         # any processing required before saving the data file
         # select only polygons we want
         gdf_land = gdf.loc[gdf['theme'] == 'Land,']
-        print(len(gdf_land.index))
         gdf_result = gdf_land.loc[gdf_land['make'] == 'Multiple']
         gdf_land = gdf_land.loc[gdf_land['make'] == 'Manmade']
-        print(len(gdf_result.index))
 
         # buildings
         gdf_blds = gdf.loc[gdf['descriptive_group'] == 'Building,']
-        print(len(gdf_blds.index))
         gdf_blds = gdf_blds.loc[gdf_blds['make'] == 'Manmade']
-        print(len(gdf_blds.index))
 
         # rail
         gdf_rail = gdf.loc[gdf['descriptive_group'] == 'Rail,']
-        print(len(gdf_rail.index))
         gdf_rail = gdf_rail.loc[gdf_rail['make'] == 'Manmade']
-        print(len(gdf_rail.index))
 
         # roads
         gdf_roads_ = gdf.loc[gdf['descriptive_group'] == 'Road Or Track,']
-        print(len(gdf_roads_.index))
         gdf_roads = gdf_roads_.loc[gdf_roads_['make'] == 'Manmade']
         gdf_roads.append(gdf_roads_.loc[gdf_roads_['make'] == 'Unknown'])
-        print(len(gdf_roads.index))
 
         # roadside
         gdf_roadside = gdf.loc[gdf['descriptive_group'] == 'Roadside,']
         gdf_roadside = gdf_roadside.loc[gdf_roadside['make'] == 'Natural']
-        print(len(gdf_roadside.index))
 
         # add all layers together
         gdf_result = gdf_result.append(gdf_land).append(gdf_blds).append(gdf_rail).append(gdf_roads).append(gdf_roadside)
@@ -235,7 +230,7 @@ def main():
         #gdf['res_count'] = gdf['res_count'].astype('int64')
         #gdf.drop([''], axis=1)
 
-        print('Saving output')
+        print('Information: Saving output - %s' %output_name)
         gdf_result.to_file(os.path.join(output_dir,'%s.gpkg' %output_name), driver="GPKG")
 
     elif feature_layer == 'developed-land-less':
@@ -246,11 +241,11 @@ def main():
         classification_codes, area_scale, data_year)
 
         # download data and build geodataframe
-        gdf = download_data(query=rstring, area_codes=area_codes, auth_username=auth_username, auth_password=auth_password)
+        gdf = download_data(query=rstring, area_codes=area_codes, auth_username=auth_username, auth_password=auth_password, debug=debug)
 
-        print(gdf.columns)
+        if debug: print(gdf.columns)
         #print(gdf.head)
-        print(len(gdf.index))
+        if debug: print(len(gdf.index))
 
         # any processing required before saving the data file
         # select only polygons we want
@@ -262,15 +257,11 @@ def main():
 
         # buildings
         gdf_blds = gdf.loc[gdf['descriptive_group'] == 'Building,']
-        print(len(gdf_blds.index))
         gdf_blds = gdf_blds.loc[gdf_blds['make'] == 'Manmade']
-        print(len(gdf_blds.index))
 
         # rail
         gdf_rail = gdf.loc[gdf['descriptive_group'] == 'Rail,']
-        print(len(gdf_rail.index))
         gdf_rail = gdf_rail.loc[gdf_rail['make'] == 'Manmade']
-        print(len(gdf_rail.index))
 
         # roads
         #gdf_roads_ = gdf.loc[gdf['descriptive_group'] == 'Road Or Track,']
@@ -292,7 +283,7 @@ def main():
         #gdf['res_count'] = gdf['res_count'].astype('int64')
         #gdf.drop([''], axis=1)
 
-        print('Saving output')
+        print('Information: Saving output - %s' %output_name)
         gdf_result.to_file(os.path.join(output_dir,'%s.gpkg' %output_name), driver="GPKG")
 
 main()
